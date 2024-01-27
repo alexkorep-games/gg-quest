d924d751712ddf66cdec9823a2a527becf0d4fd6
Alexey Korepanov <alexkorep@gmail.com>
2024-01-27 15:12:55 +0100

    Add teleport animation

--- src/scenes/level_trigger/level_trigger.gd
+++ src/scenes/level_trigger/level_trigger.gd
@@ -16,10 +16,16 @@ func _ready():
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
 		if next_level_scene_path != "":
-			var next_level_scene = load(next_level_scene_path)
-			if next_level_scene:
-					get_tree().change_scene_to(next_level_scene)
-			else:
-					print("Unable to load the scene: " + next_level_scene_path)
+			body.hide()
+			get_node("%AnimationPlayer").play("teleport")
 	else:
 			print("Next level scene path not set.")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "teleport":
+		var next_level_scene = load(next_level_scene_path)
+		if next_level_scene:
+			get_tree().change_scene_to(next_level_scene)
+		else:
+			print("Unable to load the scene: " + next_level_scene_path)
